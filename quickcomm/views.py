22d65834d378cecc5e4f2517f17a40a7dd096e88
Rajan Maghera <rmaghera@ualberta.ca>
2023-02-06 21:06:22 -0700

    Create base index view

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from quickcomm.forms import CreateMarkdownForm, CreatePlainTextForm
+from quickcomm.models import Author, Post
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'quickcomm/index.html', context)
+
