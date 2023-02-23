087a8cd14b2f3fa2ec7c0b189009540c98eb26e6
Jordan <jdvandenbruel@gmail.com>
2023-02-23 16:48:14 -0700

    Cleanup admin approval implementation

--- quickcomm/admin.py
+++ quickcomm/admin.py
@@ -2,22 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Post, Comment, Follow, Like
-from .models import RegistrationSettings
-from .forms import RegistrationSettingsForm
+from .models import Author, Post, Comment, Follow, Like, RegistrationSettings
 
 # allow admin to make new accounts inactive
-
-
-class RegistrationSettingsAdmin(admin.ModelAdmin):
-    form = RegistrationSettingsForm
-
-
-admin.site.register(RegistrationSettings, RegistrationSettingsAdmin)
-
 
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Follow)
 admin.site.register(Like)
+admin.site.register(RegistrationSettings)
