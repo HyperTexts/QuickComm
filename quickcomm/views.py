1383140a1b3cef8adaa2d642f9a4f71aff6fd2e2
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-02 13:59:36 -0700

    Add create image UI

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -1,8 +1,9 @@
+from django import template
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from quickcomm.forms import CreateMarkdownForm, CreatePlainTextForm, CreateLoginForm
+from quickcomm.forms import CreateImageForm, CreateMarkdownForm, CreatePlainTextForm, CreateLoginForm
 from quickcomm.models import Author, Post
 
 # Create your views here.
@@ -10,7 +11,8 @@ from quickcomm.models import Author, Post
 
 def index(request):
     context = {
-        'posts': Post.objects.all()
+        'posts': Post.objects.all(),
+        'request': request
     }
     return render(request, 'quickcomm/index.html', context)
 
@@ -29,6 +31,12 @@ def create_post(request):
         form = CreatePlainTextForm()
     return render(request, 'quickcomm/create.html', {'form': form, 'post_type': 'plain text'})
 
+r = template.Library()
+@r.filter(name='get_image')
+def get_image(post, request):
+    return Post.get_image_url(post, request)
+
+
 
 @login_required
 def create_markdown(request):
@@ -44,6 +52,17 @@ def create_markdown(request):
         form = CreateMarkdownForm()
     return render(request, 'quickcomm/create.html', {'form': form, 'post_type': 'CommonMark Markdown'})
 
+@login_required
+def create_image(request):
+    if request.method == 'POST':
+        form = CreateImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            author = Author.objects.get(user=request.user)
+            form.save(author)
+            return HttpResponseRedirect('/')
+    else:
+        form = CreateImageForm()
+    return render(request, 'quickcomm/create.html', {'form': form, 'post_type': 'image'})
 
 def login(request):
     if request.method == 'POST':
