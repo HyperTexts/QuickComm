f17e0a627af231ac2f5c1bed8d2b7fe29e27dd9f
Jordan <jdvandenbruel@gmail.com>
2023-03-02 15:18:08 -0700

    create example test for ViewFollowers

--- quickcomm/tests/test_views.py
+++ quickcomm/tests/test_views.py
@@ -46,15 +46,14 @@ class LogoutViewTest(TestCase):
 class EditProfileViewTest(TestCase):
     def setUp(self):
         
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
             username='user',
             password='pass'
         )
+
         
-        user.save()
-        
-        author = Author.objects.create(user=user, host='http://127.0.0.1:8000', display_name='First Name', github='https://github.com/', profile_image='https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTU3ODc5MDg2NDM2NjU2NDU3/reagan_flags.jpg')
-        author.full_clean()
+        self.author = Author.objects.create(user=self.user, host='http://127.0.0.1:8000', display_name='First Name', github='https://github.com/', profile_image='https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTU3ODc5MDg2NDM2NjU2NDU3/reagan_flags.jpg')
+        self.author.save()
         
         
     def test_edit_not_logged_in(self):
@@ -96,5 +95,47 @@ class FollowersTest(TestCase):
         second_user.save()
     def test_follower(self):
         pass
+
+class ViewFollowersTest(TestCase):
+    def setUp(self):
+        
+        self.user1 = User.objects.create_user(
+            username='user1',
+            password='pass1'
+        )
+
+        self.user2 = User.objects.create_user(
+            username='user2',
+            password='pass2'
+        )
+
+        
+        self.author1 = Author.objects.create(user=self.user1, host='http://127.0.0.1:8000', display_name='user1', github='https://github.com/', profile_image='https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTU3ODc5MDg2NDM2NjU2NDU3/reagan_flags.jpg')
+        self.author1.save()
+
+        self.author2 = Author.objects.create(user=self.user2, host='http://127.0.0.1:8000', display_name='user2', github='https://github.com/', profile_image='https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTU3ODc5MDg2NDM2NjU2NDU3/reagan_flags.jpg')
+        self.author2.save()
+
+        self.follow = Follow.objects.create(follower=self.author2, following=self.author1)
+
+    def testViewFollowers(self):
+        c = Client()
+        
+        response = self.client.get('/authors/'+str(self.author1.id)+'/followers/')
+        self.assertEqual(response.status_code, 200)
+
+        # check to make sure it is user 1's page and user 2 appears as a follower
+        find_user_2 = str(response.content).find('<h3 class=""> user2 </h3>')
+        self.assertTrue(find_user_2 > -1)
+        
+        find_user_1 = str(response.content).find('<h1>user1\\\'s Followers</h1>')
+        self.assertTrue(find_user_1 > -1, response.content)
+
+        miss_user_2 = str(response.content).find('<h1>user2\\\'s Followers</h1>')
+        self.assertTrue(miss_user_2 == -1)
+        
+        miss_user_1 = str(response.content).find('<h3 class=""> user1 </h3>')
+        self.assertTrue(miss_user_1 == -1)
+
 
     
