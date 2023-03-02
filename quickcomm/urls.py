f31431c2cd5b7c0105c3e431ba9602fa78d9f6c8
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-02 13:59:50 -0700

    Update urls.py

--- quickcomm/urls.py
+++ quickcomm/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('create/plain', views.create_post, name='create_plain'),
     path('create/markdown', views.create_markdown, name='create_markdown'),
+    path('create/image', views.create_image, name='create_image'),
 ]
