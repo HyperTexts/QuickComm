241a4c227cf0b737ffca559f169ed51bc662ea4a
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-02 22:59:39 -0700

    Add view methods for followed GH users

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -1,3 +1,4 @@
+from dateutil import parser
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
@@ -5,16 +6,42 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.contrib.auth.models import User
 
 from quickcomm.forms import CreateMarkdownForm, CreatePlainTextForm, CreateLoginForm, CreateRegisterForm
-from quickcomm.models import Author, Inbox
+from quickcomm.models import Author, Follow, Inbox
+from .external_requests import get_github_stream
 
 # Create your views here.
 
 @login_required
 def index(request):
+
+    # Get the author object for the current user
     author = Author.objects.get(user=request.user)
-    inbox = Inbox.objects.filter(author=author).order_by('-added')
+    inbox = list(Inbox.objects.filter(author=author).order_by('-added'))
+
+    # Get the GitHub stream for all of the author's followed users
+    following = [ follow.following for follow in Follow.objects.filter(follower=author) ]
+    following.append(author)
+    for follow in following:
+        github = get_github_stream(follow.github)
+        github = [dict(item, **{
+            'format': 'github',
+            'localAuthor': Author.objects.all()[0],
+            'added': parser.parse(item["created_at"])
+                                }) for item in github]
+        inbox.extend(github)
+
+    def get_date(item):
+        """Get the date of the item, regardless of whether it's a dict or an object."""
+        if isinstance(item, dict):
+            return item['added']
+        else:
+            return item.added
+
+    # Sort the inbox by date, most recent first
+    inbox.sort(key=lambda x: get_date(x), reverse=True)
+
     context = {
-        'inbox': inbox
+        'inbox': inbox,
     }
     return render(request, 'quickcomm/index.html', context)
 
