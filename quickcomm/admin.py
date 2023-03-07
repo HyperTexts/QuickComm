2c64a326851e274b737288733b957bf4778618fe
ivanseit2001 <77692029+ivanseit2001@users.noreply.github.com>
2023-03-06 20:12:13 -0700

    Registered requests
    Signed-off-by: ivanseit2001 <77692029+ivanseit2001@users.noreply.github.com>

--- quickcomm/admin.py
+++ quickcomm/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Post, Comment, Follow, Like,follow_request, RegistrationSettings, Inbox
+from .models import Author, Post, Comment, Follow, Like, RegistrationSettings, Inbox,follow_request
 
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Follow)
 admin.site.register(Like)
-admin.site.register(follow_request)
 admin.site.register(Inbox)
 admin.site.register(RegistrationSettings)
+admin.site.register(follow_request)
