42f516230bfa5aada648c696be351f1692dbe8bd
Micheal Chow <micheal.chow672@gmail.com>
2023-03-13 09:15:50 -0600

    set-up for heroku deployment

--- quickcommproj/settings.py
+++ quickcommproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,3 +169,5 @@ SWAGGER_SETTINGS = {
     'DEFAULT_AUTO_SCHEMA_CLASS': 'quickcomm.swagger.CompoundTagsSchema',
 
  }
+
+django_on_heroku.settings(locals())
