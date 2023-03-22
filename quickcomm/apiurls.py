7cc084ccfba1fcaa1ad16018a6114535e5d49424
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-22 03:38:48 -0600

    Add new urls for new api endpoints

--- quickcomm/apiurls.py
+++ quickcomm/apiurls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 
-from quickcomm.routers import AuthorRouter, PostRouter
+from quickcomm.routers import AuthorLikedRouter, AuthorRouter, CommentsRouter, InboxRouter, PostLikesRouter, PostRouter, FollowersRouter
 from . import api
 
 
@@ -11,10 +11,34 @@ from . import api
 author = AuthorRouter()
 author.register(r'authors', api.AuthorViewSet)
 
+liked = AuthorLikedRouter(author, r'authors', lookup='authors')
+liked.register(r'liked', api.AuthorLikedViewSet, basename='liked')
+
 post = PostRouter(author, r'authors', lookup='authors')
 post.register(r'posts', api.PostViewSet)
+
+postlikes = PostLikesRouter(post, r'posts', lookup='posts')
+postlikes.register(r'likes', api.PostLikesViewSet, basename='likes')
+
+follower = FollowersRouter(author, r'authors', lookup='authors')
+follower.register(r'followers', api.FollowerViewSet)
+
+comment = CommentsRouter(post, r'posts', lookup='posts')
+comment.register(r'comments', api.CommentViewSet)
+
+commentlikes = PostLikesRouter(comment, r'comments', lookup='comments')
+commentlikes.register(r'likes', api.CommentLikesViewSet, basename='likes')
+
+inbox = InboxRouter(author, r'authors', lookup='authors')
+inbox.register(r'inbox', api.InboxViewSet)
 
 urlpatterns = [
     path('', include(author.urls)),
     path('', include(post.urls)),
+    path('', include(follower.urls)),
+    path('', include(comment.urls)),
+    path('', include(inbox.urls)),
+    path('', include(postlikes.urls)),
+    path('', include(commentlikes.urls)),
+    path('', include(liked.urls)),
 ]
