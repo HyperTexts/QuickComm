9c9bc1a33bd938efba008a1723a08d4d4328e0c6
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-22 03:38:21 -0600

    Add new routers

--- quickcomm/routers.py
+++ quickcomm/routers.py
@@ -2,7 +2,7 @@
 from rest_framework import routers
 from rest_framework.routers import Route
 from rest_framework_nested import routers as nested_routers
-
+from django.urls import path
 # This file contains the routers for the API. Routers are used to map HTTP methods
 # to viewsets. They are similar to urls.py, but they are more flexible and allow
 # for more complex mappings.
@@ -34,6 +34,128 @@ class AuthorRouter(routers.SimpleRouter):
             initkwargs={'suffix': 'Instance'}
         ),
     ]
+
+class FollowersRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        # Detail route.
+        # FIXME HARDCODED, BAD! DON'T DO THIS
+        Route(
+            url=r'^{prefix}/(?P<pk>.+){trailing_slash}$',
+            mapping={
+                'get': 'retrieve',
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Instance'}
+        ),
+    ]
+
+# TODO paginate comment router
+class CommentsRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+                'post': 'create'
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        # Detail route.
+        Route(
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
+            mapping={
+                'get': 'retrieve',
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Instance'}
+        ),
+    ]
+
+class InboxRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'post': 'create',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+    ]
+
+class PostLikesRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+    ]
+
+class CommentLikesRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+    ]
+
+class AuthorLikedRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+    ]
+class LikedRouter(nested_routers.NestedSimpleRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+    ]
+
+
 
 class PostRouter(nested_routers.NestedSimpleRouter):
     routes = [
