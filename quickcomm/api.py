e8164efb47f63e98e08a43127572804998863cbc
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-22 03:36:27 -0600

    Update methods for PostViewSet

--- quickcomm/api.py
+++ quickcomm/api.py
@@ -179,30 +179,22 @@ class PostViewSet(viewsets.ModelViewSet):
     # these should also return 404s when not found, rather than
     # 403s
 
-    # Sample code taken from decorator tutorial: https://realpython.com/primer-on-python-decorators/
-    def verify_same_author(func):
-        """This is a decorator that checks that the user is authenticated and that the author matches the logged in user.
-        This is similar to middleware, but it is applied to a specific function. We can use this to reuse the same code
-        and ensure it is well-written."""
-        def wrapper(self, request, *args, **kwargs):
-            # check that the user is authenticated
-            if not request.user.is_authenticated:
-                return Response(status=403)
-
-            # check that the author matches the logged in use
-            author = Author.objects.get(user=request.user)
-            if author.id != self.kwargs['authors_pk']:
-                return Response(status=403)
-            return func(self, request, *args, **kwargs)
-        return wrapper
-
     def get_queryset(self):
         """Returns posts for this specific author."""
+
+        # TODO use reverse
+        self.paginator.url = self.request.build_absolute_uri()
 
         #check if authors_pk exists in kwargs
         if 'authors_pk' not in self.kwargs:
             return Post.objects.none()
-        return Post.objects.filter(author=self.kwargs['authors_pk'])
+
+        self.paginator.upper_response_param = 'author'
+        self.paginator.upper_url = self.request.build_absolute_uri(reverse('author-detail', kwargs={'pk': self.kwargs['authors_pk']}))
+        try:
+            return Post.objects.filter(author=self.kwargs['authors_pk'])
+        except:
+            raise exceptions.NotFound('Author not found')
 
     @swagger_auto_schema(
             operation_summary="Get a list of all posts for a specific author.",
@@ -210,10 +202,10 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: PostsSerializer, 404: "Author not found"},
     )
 
+    @authAPI
     def list(self, request, authors_pk=None):
-        queryset = self.get_queryset()
-        serializer = PostSerializer(queryset, many=True, context={'request': request})
-        return Response({'type': 'posts', 'data': serializer.data})
+        return super(PostViewSet, self).list(request)
+
 
     @swagger_auto_schema(
             operation_summary="Create a new post.",
@@ -221,7 +213,8 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: "Success", 404: "Author not found", 403: "Not authorized"},
             security=[{"BasicAuth": []}],
     )
-    @verify_same_author
+    @authAPI
+    @authAuthor
     def create(self, request, authors_pk=None):
         super().create(request)
         return Response(status=200)
@@ -232,7 +225,8 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: "Success", 404: "Author not found", 403: "Not authorized"},
             security=[{"BasicAuth": []}],
     )
-    @verify_same_author
+    @authAPI
+    @authAuthor
     def partial_update(self, request, *args, **kwargs):
         super().partial_update(request, *args, **kwargs)
         return Response(status=200)
@@ -243,9 +237,10 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: "Success", 404: "Post not found", 403: "Not authorized"},
             security=[{"BasicAuth": []}],
     )
-    @verify_same_author
+    @authAPI
+    @authAuthor
     def destroy(self, request, *args, **kwargs):
-        super().destroy(request, *args, **kwargs)
+        super().destroy(self, request, *args, **kwargs)
         return Response(status=200)
 
     @swagger_auto_schema(
@@ -254,6 +249,7 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: "Success", 404: "Post not found"},
             security=[],
     )
+    @authAPI
     def image(self, request, authors_pk=None, pk=None):
         post = get_object_or_404(Post, pk=pk)
         if post.content_type != Post.PostType.PNG and post.content_type != Post.PostType.JPG:
@@ -267,7 +263,8 @@ class PostViewSet(viewsets.ModelViewSet):
             responses={200: "Success", 404: "Post not found", 403: "Not authorized"},
             security=[{"BasicAuth": []}],
     )
-    @verify_same_author
+    @authAPI
+    @authAuthor
     def update(*args, **kwargs):
         return super().update(*args, **kwargs)
 
@@ -276,8 +273,17 @@ class PostViewSet(viewsets.ModelViewSet):
             operation_description="This endpoint returns the details of a specific post.",
             responses={200: PostSerializer, 404: "Post not found"},
     )
-    def retrieve(*args, **kwargs):
-        return super().retrieve(*args, **kwargs)
+    @authAPI
+    def retrieve(self, *args, **kwargs):
+        return super().retrieve(self, *args, **kwargs)
+
+    @authAPI
+    def likes(self, request, authors_pk=None, pk=None):
+        post = get_object_or_404(Post, pk=pk)
+        queryset = post.likes.all()
+        serializer = LikeActivitySerializer(queryset, many=True, context={'request': request})
+        return Response({'type': 'likes', 'items': serializer.data})
+
 
 
 class CommentViewSet(viewsets.ModelViewSet):
