08b02db32cccc2862980cf4cd9f23c7da79934a6
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-22 03:33:47 -0600

    Move logic for deserializer

--- quickcomm/external_host_deserializers.py
+++ quickcomm/external_host_deserializers.py
@@ -6,41 +6,6 @@
 # to other servers can then be made independently of our internal calls.
 
 # TODO make sure that our API only return local hosts
+# TODO what do we do when we see authors that aren't on any of our servers or a host we know about?
 
-from quickcomm.models import Author, Host
 
-
-def deserialize_author(item, host=None):
-    """Deserialize an author from a dictionary."""
-
-    # get host from item
-    # FIXME fix for trailing slashes
-    host_url = item["host"]
-
-    # check if host exists
-    if host is None:
-        host = Host.objects.get(url=item["host"])
-        if host is None:
-            raise Exception(f"Host {host_url} does not exist.")
-
-    # strip id of slashes
-    # TODO should we store the ID or the url?
-    # id = item["id"].split("/")[-1]
-
-    # check if author exists
-    external_url = item["url"]
-    author = Author.objects.get(external_url=external_url)
-    if author is not None:
-        # update author with new data
-        author.display_name = item["displayName"]
-        author.github = item.get("github", None)
-        author.profile_image = item.get("profileImage", None)
-        return author
-
-    return Author(
-        external_url=item["url"],
-        host=host,
-        display_name=item["displayName"],
-        github=item.get("github", None),
-        profile_image=item.get("profileImage", None),
-    )
