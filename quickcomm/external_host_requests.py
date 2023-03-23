92236cb81737da77c159449026685be2b22ff192
Rajan Maghera <rmaghera@ualberta.ca>
2023-03-23 14:39:18 -0600

    Create base requests class
    All request "serializers" branch from this class

--- quickcomm/external_host_requests.py
+++ quickcomm/external_host_requests.py
@@ -6,26 +6,35 @@
 # and caching external requests.
 
 import requests_cache
+import logging
 
 # This caches the requests for 5 minutes, but this can be changed
 session = requests_cache.CachedSession('external_cache', expire_after=300)
 
-def get_authors(url):
-    """Get all author details from a host. This function will raise an exception
-    if the request fails."""
+class BaseQCRequest:
+    """This class is used to make requests to other servers. It is used to
+    make requests to other servers, and to cache the results of those requests.
+    This class is meant to be extended by other classes that will make requests
+    to specific servers."""
 
-    # clean up the URL
-    if url[-1] == '/':
-        url = url[:-1]
+    AUTHORS_ENDPOINT = '/authors'
+    POSTS_ENDPOINT = '/posts'
+    COMMENTS_ENDPOINT = '/comments'
+    POST_LIKES_ENDPOINT = '/likes'
+    COMMENT_LIKES_ENDPOINT = '/likes'
+    FOLLOWERS_ENDPOINT = '/followers'
 
-    # get the authors
-    empty = False
-    authors = []
-    page = 1
-    while not empty:
+    def __init__(self, host, deserializers):
+        """Initialize the request object. This will set the host and the
+        session to use for the requests."""
 
-        # call api
-        response = session.get(f'{url}/authors', params={'page': page, 'size': 5})
+        self.host_obj = host
+        self.host = host.url
+        if self.host[-1] == '/':
+            self.host = self.host[:-1]
+        self.session = session
+        self.auth = host.username_password_base64
+        self.deserializers = deserializers
 
         # check response code
         if response.status_code != 200:
