45af35d2cb57b48224236bd1766bedea8778134b
Micheal Chow <micheal.chow672@gmail.com>
2023-03-30 13:30:58 -0600

    removed duplicate lines

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -194,26 +194,19 @@ def logout(request):
 
 def register(request):
     if request.method == 'POST':
-            form = UserCreationForm(request.POST)
-            if form.is_valid():
-                user = form.save()
-                author = Author(user=user, host='http://127.0.0.1:8000', display_name=user, github='https://github.com/', profile_image='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
-                author.save()
-                # either log the user in or set their account to inactve
-                admin_approved = RegistrationSettings.objects.first().are_new_users_active
-                if admin_approved:
-                    auth_login(request, user)
-                else:
-                    user.is_active = False
-                    user.save()
-                # either log the user in or set their account to inactve
-                admin_approved = RegistrationSettings.objects.first().are_new_users_active
-                if admin_approved:
-                    auth_login(request, user)
-                else:
-                    user.is_active = False
-                    user.save()
-                return redirect('/')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            author = Author(user=user, host='http://127.0.0.1:8000', display_name=user, github='https://github.com/', profile_image='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+            author.save()
+            # either log the user in or set their account to inactve
+            admin_approved = RegistrationSettings.objects.first().are_new_users_active
+            if admin_approved:
+                auth_login(request, user)
+            else:
+                user.is_active = False
+                user.save()
+            return redirect('/')
     else:
             form = UserCreationForm()
     context = {
