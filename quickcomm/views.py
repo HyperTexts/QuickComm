2f12d58171d51a9221406a498e08b4be61000de5
Nanaya <seit@ualberta.ca>
2023-03-31 14:00:57 -0600

    Something

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -383,6 +383,7 @@ def share_public_post(request,author_id,post_id):
         messages.success(request,"The author have set the post private")
         return redirect('view_author_posts',author_id=author_id)
     else:
+        print(post.visibility)
         print(request.get_host()+"/authors/"+str(author_id)+"/posts/"+str(post_id))
         print(post.source)
         new_post=Post.objects.create(
