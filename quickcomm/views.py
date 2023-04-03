856fd61df5bb8d9785247c62deacb8e010def5c4
Rajan Maghera <rmaghera@ualberta.ca>
2023-04-02 21:14:05 -0600

    Remove unlisted items from all public posts

--- quickcomm/views.py
+++ quickcomm/views.py
@@ -633,7 +633,7 @@ def all_posts(request):
         if author.is_remote and not author.is_temporary:
             sync_posts(author)
 
-    posts = Post.objects.filter(visibility=Post.PostVisibility.PUBLIC).order_by('-published')
+    posts = Post.objects.filter(visibility=Post.PostVisibility.PUBLIC, unlisted=False).order_by('-published')
 
     size = request.GET.get('size', '10')
     paginator = Paginator(posts, size)
